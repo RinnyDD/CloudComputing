adb9d7f1d25982a270f69b7ef9fdea723ccbd778
AllanzoDaizen <soksovannara098@gmail.com>
2025-07-15 21:28:45 +0700

    Change smth

--- models/db.js
+++ models/db.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: process.env.DB_PORT || 3306, // Default MySQL port
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 });
